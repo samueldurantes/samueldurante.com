b74577020302738bbe754211718d72e37586ec89
Samuel Durante <samueldurante.h@gmail.com>
2023-06-30 23:05:30 +0000

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://samueldurantes.github.io/blog',
+  generateRobotsTxt: true,
+  trailingSlash: true,
+  outDir: './out',
+  exclude: [],
+};

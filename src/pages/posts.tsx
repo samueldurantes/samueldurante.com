a6675740c71f11312fd4d002f715f38a07bd734f
Samuel Durante <samueldurante.h@gmail.com>
2023-08-22 00:37:42 +0000

    improve ui

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,7 +1,6 @@
 import type { NextPage, GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
-import moment from 'moment';
 
 import Header from '../components/Header';
 import { getAllPosts } from '../../lib/posts';
@@ -11,7 +10,7 @@ type Props = {
   posts: Posts;
 };
 
-const Home: NextPage<Props> = ({ posts: _posts }) => {
+const PostsPage: NextPage<Props> = ({ posts: _posts }) => {
   const posts = _posts.sort((a, b) => {
     if (a.metadata.created_at < b.metadata.created_at) {
       return 1;
@@ -34,7 +33,7 @@ const Home: NextPage<Props> = ({ posts: _posts }) => {
         <Header />
 
         <div className="divide-y">
-          <h1 className="pt-10 pb-5 text-2xl">Posts:</h1>
+          <h1 className="pt-10 pb-5 text-2xl">All posts:</h1>
           <p className="py-4">No posts found.</p>
         </div>
       </div>
@@ -50,42 +49,24 @@ const Home: NextPage<Props> = ({ posts: _posts }) => {
       <Header />
 
       <div className="divide-y">
-        <h1 className="pt-10 pb-5 text-2xl">Posts:</h1>
-        {posts.slice(0, 4).map((post: Post, key: number) => (
-          <div className="flex flex-col gap-1 py-10" key={key}>
-            <p className="text-sm" key={key}>
-              {moment(post.metadata.created_at).format('ll')}
-            </p>
+        <h1 className="pt-10 pb-5 text-2xl">All posts:</h1>
+        <div className="flex flex-col gap-3 py-4">
+          {posts.map((post: Post, key: number) => (
             <Link
-              className="text-xl text-blue-800 hover:underline"
+              className="text-base text-blue-800 hover:underline"
               href={`/post/${post.metadata.slug}`}
+              key={key}
             >
               {post.metadata.title}
             </Link>
-            <div className="flex gap-1" key={key}>
-              {post?.metadata?.tags?.map((tag: string, key: number) => (
-                <p
-                  className="italic text-sm font-serif"
-                  key={key}
-                >{`#${tag}`}</p>
-              ))}
-            </div>
-          </div>
-        ))}
-        <div className="flex justify-center">
-          <Link
-            className="text-sm text-blue-800 hover:underline py-4"
-            href="/posts"
-          >
-            See all posts
-          </Link>
+          ))}
         </div>
       </div>
     </div>
   );
 };
 
-export default Home;
+export default PostsPage;
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const posts = await getAllPosts();

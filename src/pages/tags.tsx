5c1ef3e244eb14b58308458297546bbbb07f38b8
Samuel Durante <samueldurante.h@gmail.com>
2023-12-31 00:36:52 +0000

    fix ci

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,11 +1,9 @@
 import type { NextPage, GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
-import moment from 'moment';
 
 import Header from '../components/Header';
 import { getAllPosts } from '../../lib/posts';
-import type { Posts, Post } from '../../lib/posts';
 
 type Props = {
   tags: string[];

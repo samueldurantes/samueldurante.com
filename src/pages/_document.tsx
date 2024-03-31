3ec80fb59981139f8e6edf47611ed4c3edd19150
Samuel Durante <samueldurante.h@gmail.com>
2024-03-31 00:02:23 +0000

    ga

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const MyDocument = () => {
   return (
     <Html>
-      <Head lang="en" />
+      <Head lang="en">
+        <meta name="google-site-verification" content="mo_sm3-KOvH2XQJPIqRAd4Mo2lWAD4fJbvId0jGfd70" />
+      </Head>
       <body>
         <Main />
         <NextScript />

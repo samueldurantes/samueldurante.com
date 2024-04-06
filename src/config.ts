9a0a02c26989926a5a734cf0e32578e5241fc82b
Samuel Durante <samueldurante.h@gmail.com>
2024-04-06 17:40:36 +0000

    fixs urls

--- src/config.ts
+++ src/config.ts
@@ -6,5 +6,6 @@ export const SITE = {
   author: 'Samuel Durante <samueldurante.h@gmail.com>',
 };
 
-export const GITHUB_REPO_URL = 'https://github.com/samueldurante/blog';
-export const TWITTER_URL = 'https://twitter.com/samueldurante';
+export const GITHUB_REPO_URL =
+  'https://github.com/samueldurantes/samueldurantes.github.io';
+export const TWITTER_URL = 'https://twitter.com/samueldurantes';
